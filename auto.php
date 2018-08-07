4288477a3d65491a6216ea485ff9865780d61722
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2018-08-07 22:59:56 +0200

    Fix bug

--- auto.php
+++ auto.php
@@ -1,16 +1,14 @@
 <?php
-include('antesh.php');
+include('linker.php');
 
 if($argv[1] != ""){
 	$ticker = $argv[1];
 	echo $ticker . "\n";
 	
 	echo "pystocklib...\n";
-	$pystocklib_data = new Antesh('C:\xampp\htdocs\deepforcasting\pystocklib\auto.py', array($ticker));
-	echo "pystocklib Finished!\n";
+	$pystocklib_data = new Linker('.\pystocklib\auto.py', array($ticker));
 
 	echo "StockNN...\n";
-	$StockNN_data = new Antesh('C:\xampp\htdocs\deepforcasting\StockNN\auto.py', array($ticker,'--retrain'));
-	echo "StockNN Finished!\n";
+	$StockNN_data = new Linker('.\StockNN\auto.py', array($ticker,'--retrain'));
 }
 ?>
