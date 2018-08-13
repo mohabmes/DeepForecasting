618023da80ffecd2ed99c1c153946af2803efc53
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2018-08-13 09:36:05 +0200

    Add helper function

--- StockNN/auto.py
+++ StockNN/auto.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import json
+import preprocess as prep
+import helper as hlp
 
-with open('config.json') as f:
-    config = json.load(f)
+config = hlp.load_config()
 
 model_type_file = ""
 model_type = ""
@@ -20,82 +21,60 @@ if sys.argv[0] and sys.argv[1] and sys.argv[2] != "":
     path = path.format(stock_name)
     model_type_file = path + "model.txt"
     print(path)
-    
-if not os.path.isfile(model_type_file):
-    error = "'{}' Not Found!".format(model_type_file)
-    print(error)
-    print("Determin model type ...")
-
-    print("Activate Tensorflow ...")
-    from bootstrap import determin_model
-    import preprocess as prep
-
-    print("Reading csv file ...")
-    csv_path = path + "{}.csv".format(stock_name)
-
-    print("Dataset preprocessing ...")
-    scaler, X_train, X_test, y_train, y_test = prep.preprocess(csv_path)
-
-    print("Training phase ...")
-    model_type = determin_model(stock_name, scaler, X_train, X_test, y_train, path=path, epochs=epochs_num_to_determin_model)
-    
-    f=open(model_type_file, "w")
-    f.write(model_type)
-    f.close()
-    
-else:
-    f=open(model_type_file, "r")
-    model_type = f.read()
-    print(model_type)
-
-
-import preprocess as prep
-
-if model_type == "RNN":
-    import RNN as dl
-elif model_type == "RBF":
-    import RBF as dl
-elif model_type == "BKP":
-    import BKP as dl
-
 
 if not os.path.exists(path):
     error = "error : '{}' Not Found!".format(path)
     print(error)
     exit()
 
-
+print("Reading csv file ...")
 csv_path = path + "{}.csv".format(stock_name)
 
 print("Dataset preprocessing ...")
 scaler, X_train, X_test, y_train, y_test = prep.preprocess(csv_path)
 
-X = X_test[len(X_test)-1]
+    
+if not os.path.isfile(model_type_file):
+    from bootstrap import determin_model
 
-if op == "--train":
-    regressor = dl.train(X_train, y_train, epochs = epochs_num)
-    dl.save_model(stock_name, regressor, path=path)
+    print("Training phase ...")
+    model_type = determin_model(stock_name, scaler, X_train, X_test, y_train, path=path, epochs=epochs_num_to_determin_model)
+
+    hlp.write_file(model_type_file, model_type)
     
-    
-elif op == "--retrain":
-    # path of the old model
-    old_model = path + "{}-{}.h5".format(stock_name, model_type)
-    regressor = dl.retrain(old_model, X_train, y_train, epochs = epochs_num)
-    dl.save_model(stock_name, regressor, path=path)
-    
-elif op == "--test":
-    # path of the old model
-    model_path = path + "{}-{}.h5".format(stock_name, model_type)
-    _mse, _mape = dl.test(model_path, scaler, X_test)
-    print(_mse, _mape)
+else:
+    model_type = hlp.read_file(model_type_file)
+    print(model_type)
+
+    if model_type == "RNN":
+        import RNN as dl
+    elif model_type == "RBF":
+        import RBF as dl
+    elif model_type == "BKP":
+        import BKP as dl
+
+    if op == "--train":
+        regressor = dl.train(X_train, y_train, epochs = epochs_num)
+        dl.save_model(stock_name, regressor, path=path)
+
+    elif op == "--retrain":
+        # path of the old model
+        old_model = path + "{}-{}.h5".format(stock_name, model_type)
+        regressor = dl.retrain(old_model, X_train, y_train, epochs = epochs_num)
+        dl.save_model(stock_name, regressor, path=path)
+
+    elif op == "--test":
+        # path of the old model
+        model_path = path + "{}-{}.h5".format(stock_name, model_type)
+        _mse, _mape = dl.test(model_path, scaler, X_test)
+        print(_mse, _mape)
+
+
+X = X_test[len(X_test) - 1]
 
 print("prediction ...")
 model_path = path + "{}-{}.h5".format(stock_name, model_type)
 prediction = dl.predict(model_path, scaler, X)
 print(str(prediction[1]))
 
-f=open(path + "prediction.txt", "w")
-f.write(str(prediction[1]))
-f.close()
-
-
+hlp.write_file(path + "prediction.txt", str(prediction[1]))
